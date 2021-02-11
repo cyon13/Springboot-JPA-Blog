3a31192645eebba0acd612c10e06b71c8983337b
Bonyul <bonyul13@gmail.com>
2021-02-12 00:26:55 +0900

    환경설정 완료

---
+++ src/main/java/com/cyon13/web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cyon13.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogWebApplication.class);
+	}
+
+}
---
+++ src/test/java/com/cyon13/web/BlogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyon13.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
